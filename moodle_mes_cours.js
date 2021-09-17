ba5582dc285698e2af427911f4809248c8eb3601
TheRolf <yann.lv@live.fr>
2021-09-17 09:51:38 +0200

    Update moodle_mes_cours.js

--- moodle_mes_cours.js
+++ moodle_mes_cours.js
@@ -7,37 +7,51 @@
 if(!this.location.href.includes("moodle.utbm.fr"))
   return;
 
-const mescours = ['SM57', 'LR01', 'LO53', 'MG07', 'TO52', 'MI52'].sort()
+const MES_COURS_TITLE = 'Mes cours'
+const AUTRES_COURS_TITLE = 'Autres cours'
+const mescours = ['DA50', 'DA51', 'DA52', 'DA53', 'DA54', 'LE07'].sort()
+mescours.unshift(MES_COURS_TITLE)
 
-console.log(mescours)
+const LIST_ELEMENT_SELECTOR = '#nav-drawer .list-group > ul'
+const LIST_ITEM_SELECTOR = LIST_ELEMENT_SELECTOR + ' > li'
+const LIST_ELEMENT_TEXT_SELECTOR = '.media-body'
 
 window.onload = function() {
+  try {
+    const list = [ ...document.querySelectorAll(LIST_ITEM_SELECTOR)]
+    const mes = mescours.map(el => '')
 
-  const list = [ ...document.querySelectorAll('.tree_item.branch.canexpand + ul > li')]
-  
-  const mes = []
-  mescours.forEach(cour => mes.push(''))
-  
-  list.forEach(el => {
-    for(let i = 0; i < mescours.length; ++i) {
-      if(el.innerText.includes(mescours[i]))
-        mes[i] = el
-    }
-  })  
-  
-  const autres = list.filter(el => {
-    for(let i = 0; i < mescours.length; ++i) {
-      if(el.innerText.includes(mescours[i])) return false;
-    }
-    return true;
-  })
-  
-  autres.sort((a, b) => (a.innerText < b.innerText ? -1 : 1))
-  
-  const listElement = document.querySelector('.tree_item.branch.canexpand + ul');
-  listElement.innerHTML = '';
-  
-  mes.forEach(m => listElement.appendChild(m))
-  listElement.innerHTML += '<hr>'
-  autres.forEach(m => listElement.appendChild(m))
+    const autres = []
+    let added
+    list.forEach(el => {
+      added = false
+      
+      for(let i = 0; i < mescours.length; ++i) {
+        if(el.innerText.includes(mescours[i])) {
+          mes[i] = el
+          added = true
+        }
+      }
+      
+      if(!added) {
+          autres.push(el)
+      }
+      else if(el.innerText === MES_COURS_TITLE) {
+        const autreCours = el.cloneNode(true)
+        autreCours.querySelector(LIST_ELEMENT_TEXT_SELECTOR).innerText = AUTRES_COURS_TITLE;
+        autres.push(autreCours)
+      }
+    })
+
+    console.log(mes, autres)
+
+    const listElement = document.querySelector(LIST_ELEMENT_SELECTOR)
+    listElement.innerHTML = ''
+
+    mes.forEach(m => listElement.appendChild(m))
+    autres.forEach(m => listElement.appendChild(m))
+  } catch(err) {
+    console.error(err)
+    throw err
+  }
 }
